d07f65b49befe0a817b4d7b6540952db6fdb920c
HB <Harish.HB@unisys.com>
2017-03-29 17:46:02 +0530

    FC

--- Automation/src/com/ta3s/baseclass/BaseUtil.java
+++ Automation/src/com/ta3s/baseclass/BaseUtil.java
@@ -64,12 +64,12 @@ public class BaseUtil {
 			System.setProperty("webdriver.gecko.driver",
 					GetPath.getBasePath() + File.separator+"Drivers"
 							+File.separator+ "geckodriver.exe");			
-			driver = new FirefoxDriver();
+			//driver = new FirefoxDriver();
 
-			//DesiredCapabilities cap=DesiredCapabilities.firefox();
-			//cap.setCapability("version", "");
-			//cap.setCapability("platform", "LINUX");
-			//driver=new RemoteWebDriver(new URL("http://172.17.0.2:4444/wd/hub"),cap);
+			DesiredCapabilities cap=DesiredCapabilities.firefox();
+			cap.setCapability("version", "");
+			cap.setCapability("platform", "LINUX");
+			driver=new RemoteWebDriver(new URL("http:localhost:4444/wd/hub"),cap);
 
 
 		} else if (browser.equalsIgnoreCase("chrome")) {
